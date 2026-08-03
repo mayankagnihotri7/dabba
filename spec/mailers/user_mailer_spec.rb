9b76a0c45849ff0309c1e5aae7efb988c92379b3
Mayank Agnihotri <mayankagnihotri7@gmail.com>
2026-08-03 14:32:28 +0530

    fix: linting

--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -9,7 +9,7 @@ RSpec.describe UserMailer, type: :mailer do
     let(:mail) { UserMailer.otp_email(user) }
 
     it "sends to the right email with the right subject" do
-      expect(mail.to).to eq([user.email])
+      expect(mail.to).to eq([ user.email ])
       expect(mail.subject).to eq("Your Dabba code: #{user.otp_code}")
     end
 
